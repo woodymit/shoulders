4460269142c4ac105b40e88c86323e4904b4316e
csquires <csquires.mit.edu>
2015-09-20 10:24:35 -0400

    almost there

--- shoulders/static/assets/js/mvc.js
+++ shoulders/static/assets/js/mvc.js
@@ -1,74 +1,3 @@
-var width = window.innerWidth;
-var height = window.innerHeight;
-
-var foo1 = [["A0",[width*1/8,height*5/6],[7],50],
-        ["A1",[width*2/8,height*5/6],[7,8],50],
-        ["A2",[width*3/8,height*5/6],[8],50],
-        ["A3",[width*4/5,height*5/6],[8,9,10],50],
-        ["A4",[width*5/8,height*5/6],[9],50],
-        ["A5",[width*6/8,height*5/6],[11],50],
-        ["A6",[width*7/8,height*5/6],[11],50],
-        ["B0",[width*1/5,height*4/6],[12],50],
-        ["B1",[width*2/5,height*4/6],[12],50],
-        ["B2",[width*3/5,height*4/6],[12],50],
-        ["B3",[width*4/5,height*4/6],[12],50],
-        ["C0",[width/2,height/2],[15,16,17],50],
-        ["D0",[width*1/4,height*2/6],[16,17,18,19],50],
-        ["D1",[width*2/4,height*2/6],[19,20,22],50],
-        ["D2",[width*3/4,height*2/6],[20,21,22],50],
-        ["E0",[width*1/9,height*1/6],[],50],
-        ["E1",[width*2/9,height*1/6],[],50],
-        ["E2",[width*3/9,height*1/6],[],50],
-        ["E3",[width*4/9,height*1/6],[],50],
-        ["E4",[width*5/9,height*1/6],[],50],
-        ["E5",[width*6/9,height*1/6],[],50],
-        ["E6",[width*7/9,height*1/6],[],50],
-        ["E7",[width*8/9,height*1/6],[],50],
-        ]
-
-var foo2 = [["B0",[width*1/5,height*5/6],[6],50],
-        ["B1",[width*2/5,height*5/6],[6],50],
-        ["B2",[width*3/5,height*5/6],[6],50],
-        ["B3",[width*4/5,height*5/6],[6,7],50],
-        ["B4",[width*4/5,height*5/6],[7],50],
-        ["C0",[width*1/3,height*4/6],[8],50],
-        ["C1",[width*2/3,height*4/6],[8],50],
-        ["D0",[width*1/2,height*3/6],[9,10,11,12],50],
-        ["E0",[width*1/5,height*2/6],[13,14,15],50],
-        ["E1",[width*2/5,height*2/6],[15],50],
-        ["E2",[width*3/5,height*2/6],[16,17],50],
-        ["E3",[width*4/5,height*2/6],[17],50],
-        ["F0",[width*1/7,height*1/6],[],50],
-        ["F1",[width*2/7,height*1/6],[],50],
-        ["F2",[width*3/7,height*1/6],[],50],
-        ["F3",[width*4/7,height*1/6],[],50],
-        ["F4",[width*5/7,height*1/6],[],50],
-        ["F5",[width*6/7,height*1/6],[],50]
-        ]
-
-var papers1 = []
-var papers2 = []
-
-for(l in foo1) {
-    papers1.push({
-        'title': foo1[l][0],
-        'title_href': "",
-        'successors': foo1[l][2],
-        'location': foo1[l][1],
-        'radius': foo1[l][3]
-    })
-}
-for(l in foo2) {
-    papers2.push({
-        'title': foo2[l][0],
-        'title_href': "",
-        'successors': foo2[l][2],
-        'location': foo2[l][1],
-        'radius': foo2[l][3]
-    })
-}
-
-
 function getAuthorName(currentValue, i, a) {
     if (!currentValue[1]) {
         return currentValue[0]
@@ -234,10 +163,6 @@ $(document).ready(function () {
             window.location.hash = target;
         });
     });
-
-    $(document).on('click', function(){
-        renderD3(papers1);
-    });
 });
 
 
@@ -246,8 +171,8 @@ function renderD3(paperList) {
 
     //size svg
     svg = d3.select('svg')
-        .attr('width',width)
-        .attr('height',height);
+        .attr('width',window.innerWidth)
+        .attr('height',window.innerHeight);
 
     // create arrow
     svg.select('defs')
@@ -338,6 +263,7 @@ function renderD3(paperList) {
     nodeEnter = nodes.enter().append('g');
     nodeEnter.attr('class','node')
         .on('click',function(){alert("Present info");})
+        .on('mouseover',function(){})
         .attr('opacity',0)
         .attr('transform', function(d) {
             return 'translate(' + d.location[0] + ',' + d.location[1] + ')';
