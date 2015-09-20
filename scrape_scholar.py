658cf6966c8ddbe220a06078c522cf85744eaabd
woodymit <wahern@mit.edu>
2015-09-20 02:22:14 -0400

    General scraping of paper children

--- scrape_scholar.py
+++ scrape_scholar.py
@@ -8,8 +8,10 @@ import re
 SEARCH_HOST = "scholar.google.com"
 SEARCH_BASE_URL = "/scholar"
 
+RESULTS_PER_PAGE = 20
 
-def get_html(terms, limit):
+
+def search_terms(terms, limit):
     params = urllib.urlencode({'q': "+".join(terms), 'num': limit})
     headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
 
@@ -27,12 +29,50 @@ def get_html(terms, limit):
     return html.decode('ascii', 'ignore')
 
 
+def get_citers_page_html(paper):
+    url_suffix = paper.citers_page_href
+
+    headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+
+    conn = httplib.HTTPConnection(SEARCH_HOST)
+    conn.request("GET", url_suffix, '', headers)
+
+    resp = conn.getresponse()
+
+    if not resp.status == 200:
+        raise Exception('Response from Google Scholar url:' +
+                url_suffix + ' did not return with code 200')
+
+    html = resp.read()
+    return html.decode('ascii', 'ignore')
+
+
 class Paper:
-    def __init__(self, title, title_href, author_list, cited_by_href):
+    def __init__(self, title, title_href, author_list, citers_page_href):
         self.title = title
         self.title_href = title_href
         self.author_list = author_list
-        self.cited_by_href = cited_by_href
+        self.citers_page_href = citers_page_href
+
+    def __hash__(self):
+        return hash(self.title_href)
+
+
+class CitationGraph:
+    def __init__(self, center_paper):
+        self.children = {center_paper: []}
+        self.center = center_paper
+        self.newest_edge = set([center_paper])
+        self.oldest_edge = [center_paper]
+
+    def expand_newest_edge(self, iterations=1):
+        next_edge = set()
+        for parent in self.newest_edge:
+            child_list = get_citing_papers(parent)
+            self.children[parent] = child_list
+            for child in child_list:
+                if child not in self.newest_edge:
+                    next_edge.add(child)
 
 
 def parse_result(result_tag):
@@ -45,17 +85,16 @@ def parse_result(result_tag):
     else:
         title_href = None
 
-    cited_by_href = None
+    citers_page_href = None
     for a_tag in result_tag.find_all('a'):
         if re.match('Cited by [\d]+', a_tag.get_text()):
-            cited_by_href = a_tag.get('href')
+            citers_page_href = a_tag.get('href')
             break
 
     author_list = []
     authors_tag = result_tag.find_next(class_='gs_a')
     for author_html in str(authors_tag).split(','):
         author_soup = BeautifulSoup(author_html)
-        # ipdb.set_trace()
         author_link_list = author_soup.find_all('a')
         if author_link_list:
             author_href = author_link_list[0].get('href')
@@ -71,19 +110,60 @@ def parse_result(result_tag):
 
         author_list.append((author_name, author_href))
 
-    return Paper(title, title_href, author_list, cited_by_href)
+    return Paper(title, title_href, author_list, citers_page_href)
 
 
-def scrape_results_page(html):
+def get_html(rel_url):
+
+    headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+
+    conn = httplib.HTTPConnection(SEARCH_HOST)
+    conn.request('GET', rel_url, '', headers)
+
+    resp = conn.getresponse()
+
+    if not resp.status == 200:
+        raise Exception('Response from Google Scholar url:' +
+                rel_url + ' did not return with code 200')
+
+    html = resp.read()
+    return html.decode('ascii', 'ignore')
+
+
+def scrape_single_results_page(html, limit=20):
 
     paper_list = []
     soup = BeautifulSoup(html)
-    for result_tag in soup.find_all(class_='gs_ri'):
+    for result_tag in soup.find_all(class_='gs_ri')[:limit]:
         paper_list.append(parse_result(result_tag))
 
     return paper_list
 
 
-# def get_cited_papers(parent_paper):
+def scrape_results_page(html, limit=20):
 
-#     assert parent_paper.cited_by_href
+    paper_list = scrape_single_results_page(html, limit)
+    remaining = limit - RESULTS_PER_PAGE
+
+    if remaining < 0:
+        return paper_list
+
+    soup = BeautifulSoup(html)
+    next_page_url = soup.body.find('div', id='gs_top'
+        ).find('div', id='gs_bdy').find('div', id='gs_res_bdy'
+        ).find('div', id='gs_ccl').find('div', id='gs_n'
+        ).find('a').get('href')
+
+    next_page_html = get_html(next_page_url)
+
+    return paper_list + scrape_results_page(
+            next_page_html, remaining)
+
+
+def get_citing_papers(parent_paper):
+
+    assert parent_paper.citers_page_href
+    html = get_citers_page_html(parent_paper)
+    paper_list = scrape_results_page(html, 100)
+
+    return paper_list
